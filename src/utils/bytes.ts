6f3eac80e99771503563438fb17594cc44b09219
Banou <me@banou.dev>
2022-07-31 03:34:46 +0200

    refactor: ♻️getHumanReadableByteString using Intl

--- src/utils/bytes.ts
+++ src/utils/bytes.ts
@@ -4,17 +4,21 @@ export const getBytesFromBiByteString = (s: string) => {
   return Number(_number) * (2 ** ('bkmgt'.indexOf(_unit.toLowerCase()) * 10))
 }
 
-export const getHumanReadableByteString = (bytes, decimals = 2) => {
-  if (bytes === 0) return '0 Bytes'
+export const getHumanReadableByteString = (bytes) => {
+  if (bytes === 0) return '0 bytes'
   const k = 1024
   const i = Math.floor(Math.log(bytes) / Math.log(k))
 
   return (
-    `${
-      parseFloat((bytes / Math.pow(k, i))
-        .toFixed(decimals < 0 ? 0 : decimals))
-    } ${
-      ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'][i]
-    }`
+    new Intl.NumberFormat(
+      'en-US',
+      {
+        unit: ['byte', 'kilobyte', 'megabyte', 'gigabyte', 'terabyte', 'petabyte'][i],
+        notation: 'standard',
+        style: 'unit',
+        unitDisplay: 'short'
+      }
+    )
+    .format(bytes / Math.pow(k, i))
   )
 }
