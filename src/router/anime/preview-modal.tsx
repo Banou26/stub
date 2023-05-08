fbe07d92ecc6bd6a50720301832938675efb5e90
Banou <me@banou.dev>
2023-05-08 03:25:39 +0200

    feat: ✨improve preview-modal scrollbar

--- src/router/anime/preview-modal.tsx
+++ src/router/anime/preview-modal.tsx
@@ -3,7 +3,7 @@ import * as Dialog from '@radix-ui/react-dialog'
 import { useQuery } from '@apollo/client'
 import { targets } from 'laserr'
 
-import './preview-modal.css'
+// import './preview-modal.css'
 import { gql } from '../../generated'
 import { overlayStyle } from '../../components/modal'
 import { useState } from 'react'
@@ -12,147 +12,165 @@ import { MinimalPlayer } from '../../components/minimal-player'
 import { Route, getRoutePath } from '../path'
 
 const style = css`
-display: flex;
-flex-direction: column;
-width: 120rem;
-
-background-color: rgb(35, 35, 35);
-border-radius: 1rem;
-box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
-overflow: hidden;
+overflow: auto;
 position: fixed;
-top: 50%;
-left: 50%;
-transform: translate(-50%, -50%);
-animation: contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
-height: min-content;
+inset: 0;
+display: flex;
+justify-content: center;
+/* align-items: center; */
+padding: 5rem;
+/* padding-bottom: 20rem; */
+/* top: 50%;
+left: 50%; */
 
-.origin-icon {
-  margin: 0 1rem;
-}
-
-.trailer {
-  &:after {
-    content: '';
-    position: absolute;
-    inset: 0;
-    height: 67.6rem;
-    background: linear-gradient(0deg, #181818, transparent 20%);
-    pointer-events: none;
-  }
-  .volume-area-wrapper {
-    z-index: 100;
-  }
-}
-
-.player {
+.modal {
   position: relative;
-  display: grid;
-  grid-template:"container";
-  height: 67.5rem;
-  width: 100%;
+  display: flex;
+  flex-direction: column;
+  width: 120rem;
   background-color: rgb(35, 35, 35);
-  overflow: hidden;
-  user-select: none;
+  border-radius: 1rem;
+  box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
+  /* overflow: hidden; */
+  /* overflow: auto; */
+  /* position: fixed;
+  top: 50%;
+  left: 50%; */
+  /* transform: translate(0rem, 15rem); */
+  /* transform: translate(-50%, -50%); */
+  animation: contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
+  height: min-content;
+  /* max-height: 90vh; */
 
-  & > div:first-of-type {
-    grid-area: container;
-    height: 89.25rem !important;
-    width: 100% !important;
-    margin-top: -11rem;
-    pointer-events: none;
+  .origin-icon {
+    margin: 0 1rem;
   }
-}
 
-.content {
-  margin: 2.5rem;
-  
-  .title {
-    display: flex;
-    justify-content: start;
-    align-items: center;
-
-    h2 {
-      font-size: 3rem;
+  .trailer {
+    border-radius: 1rem 1rem 0 0;
+    overflow: hidden;
+    &:after {
+      content: '';
+      position: absolute;
+      inset: 0;
+      height: 67.6rem;
+      background: linear-gradient(0deg, #181818, transparent 20%);
+      pointer-events: none;
     }
+    .volume-area-wrapper {
+      z-index: 100;
+    }
+  }
 
-    .origins {
+  .player {
+    position: relative;
+    display: grid;
+    grid-template:"container";
+    height: 67.5rem;
+    width: 100%;
+    background-color: rgb(35, 35, 35);
+    overflow: hidden;
+    user-select: none;
+
+    & > div:first-of-type {
+      grid-area: container;
+      height: 89.25rem !important;
+      width: 100% !important;
+      margin-top: -11rem;
+      pointer-events: none;
+    }
+  }
+
+  .content {
+    margin: 2.5rem;
+    
+    .title {
       display: flex;
       justify-content: start;
       align-items: center;
-      margin-left: 1rem;
 
-      a {
+      h2 {
+        font-size: 3rem;
+      }
+
+      .origins {
         display: flex;
-        justify-content: center;
+        justify-content: start;
         align-items: center;
-        
-        img {
-          height: 3rem;
-          width: 3rem;
+        margin-left: 1rem;
+
+        a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          
+          img {
+            height: 3rem;
+            width: 3rem;
+          }
         }
       }
     }
-  }
 
-  .description {
-    margin-top: 2.5rem;
-  }
+    .description {
+      margin-top: 2.5rem;
+    }
 
-  .episodes {
-    margin-top: 4rem;
-    border-top: .1rem solid rgba(255, 255, 255, .1);
+    .episodes {
+      margin-top: 4rem;
+      border-top: .1rem solid rgba(255, 255, 255, .1);
 
-    .episode {
-      display: grid;
-      grid-template-columns: 5rem auto 10rem;
-      height: 10rem;
-      border-bottom: .1rem solid rgba(255, 255, 255, .1);
-      color: #fff;
-      text-decoration: none;
+      .episode {
+        display: grid;
+        grid-template-columns: 5rem auto 10rem;
+        height: 10rem;
+        border-bottom: .1rem solid rgba(255, 255, 255, .1);
+        color: #fff;
+        text-decoration: none;
 
 
-      &:hover {
-        .title {
-          text-decoration: underline;
-        }
-      }
-
-      .information, .episode-number, .date {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      .information {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        padding-left: 2.5rem;
-        /* justify-content: start; */
-
-        .title {
-          margin-right: auto;
-          font-size: 2rem;
+        &:hover {
+          .title {
+            text-decoration: underline;
+          }
         }
 
-        .description {
+        .information, .episode-number, .date {
           display: flex;
-          min-height: 2rem;
-          font-size: 2rem;
+          justify-content: center;
+          align-items: center;
         }
-      }
 
-      .episode-number {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 3rem;
-        vertical-align: middle;
+        .information {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          padding-left: 2.5rem;
+          /* justify-content: start; */
+
+          .title {
+            margin-right: auto;
+            font-size: 2rem;
+          }
+
+          .description {
+            display: flex;
+            font-size: 2rem;
+          }
+        }
+
+        .episode-number {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-size: 3rem;
+          vertical-align: middle;
+        }
       }
     }
   }
 }
+
 `
 
 export const GET_MEDIA = gql(`
@@ -247,6 +265,7 @@ export const GET_MEDIA = gql(`
   }
 `)
 
+
 export default () => {
   const [searchParams, setSearchParams] = useSearchParams()
   const mediaUri = searchParams.get('details')
@@ -285,58 +304,64 @@ export default () => {
         <Dialog.Overlay css={overlayStyle} onClick={onOverlayClick}/>
         <Dialog.Content asChild={true}>
           <div css={style}>
-            <div className="trailer">
-              <MinimalPlayer media={media} className="player"/>
-            </div>
-            <div className="content">
-              <div className="title">
-                <h2>{media?.title?.romanized}</h2>
-                <div className="origins">
+            <div className="modal">
+              <div className="trailer">
+                <MinimalPlayer media={media} className="player"/>
+              </div>
+              <div className="content">
+                <div className="title">
+                  <h2>{media?.title?.romanized}</h2>
+                  <div className="origins">
+                    {
+                      mediaTargets?.map(({ target, media }) => (
+                        <a key={target.origin} href={media.url ?? target.originUrl} className="origin-icon" target="_blank" rel="noopener noreferrer">
+                          <img src={target.icon} alt=""/>
+                        </a>
+                      ))
+                    }
+                  </div>
+                </div>
+                <div className="description" dangerouslySetInnerHTML={{ __html: media?.description }}></div>
+                <div className="episodes">
                   {
-                    mediaTargets?.map(({ target, media }) => (
-                      <a key={target.origin} href={media.url ?? target.originUrl} className="origin-icon" target="_blank" rel="noopener noreferrer">
-                        <img src={target.icon} alt=""/>
-                      </a>
-                    ))
+                    media?.airingSchedule?.edges.map(({ node }) => {
+                      const airingAt = new Date(node.airingAt * 1000)
+                      const airingAtString = airingAt.toLocaleString('en-US', { timeZone: 'UTC' })
+                      const relativeTime =
+                        !isNaN(node.timeUntilAiring) && isFinite(node.timeUntilAiring)
+                          ? new Intl.RelativeTimeFormat('en', { numeric: 'auto' }).format(Math.round(node.timeUntilAiring / 60 / 60 / 24), 'days')
+                          : undefined
+
+                      console.log('relativeTime', relativeTime)
+
+                      if (node.timeUntilAiring > 0) return undefined
+
+                      return (
+                        <Link
+                          key={node.uri}
+                          className="episode"
+                          to={getRoutePath(Route.WATCH, { uri: `${mediaUri}-${node.episode}` })}
+                        >
+                          <div className="episode-number">{node.episode}</div>
+                          {
+                            node.thumbnail
+                              ? <img src={node.thumbnail} alt="" className="thumbnail"/>
+                              : undefined
+                          }
+                          <div className="information">
+                            <div className="title">{node.title?.romanized}</div>
+                            {
+                              node.description
+                                ? <div className="description">{node.description}</div>
+                                : undefined
+                            }
+                          </div>
+                          <div className="date">{relativeTime}</div>
+                        </Link>
+                      )
+                    })
                   }
                 </div>
-              </div>
-              <div className="description" dangerouslySetInnerHTML={{ __html: media?.description }}></div>
-              <div className="episodes">
-                {
-                  media?.airingSchedule?.edges.map(({ node }) => {
-                    const airingAt = new Date(node.airingAt * 1000)
-                    const airingAtString = airingAt.toLocaleString('en-US', { timeZone: 'UTC' })
-                    const relativeTime =
-                      !isNaN(node.timeUntilAiring) && isFinite(node.timeUntilAiring)
-                        ? new Intl.RelativeTimeFormat('en', { numeric: 'auto' }).format(Math.round(node.timeUntilAiring / 60 / 60 / 24), 'days')
-                        : undefined
-
-                    console.log('relativeTime', relativeTime)
-
-                    if (node.timeUntilAiring > 0) return undefined
-
-                    return (
-                      <Link
-                        key={node.uri}
-                        className="episode"
-                        to={getRoutePath(Route.WATCH, { uri: `${mediaUri}-${node.episode}` })}
-                      >
-                        <div className="episode-number">{node.episode}</div>
-                        {
-                          node.thumbnail
-                            ? <img src={node.thumbnail} alt="" className="thumbnail"/>
-                            : undefined
-                        }
-                        <div className="information">
-                          <div className="title">{node.title?.romanized}</div>
-                          <div className="description">{node.description}</div>
-                        </div>
-                        <div className="date">{relativeTime}</div>
-                      </Link>
-                    )
-                  })
-                }
               </div>
             </div>
           </div>
