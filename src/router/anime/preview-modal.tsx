9c8d48cdcb165eb0c0b3dca9e0ba298231de21d9
Banou <me@banou.dev>
2023-06-24 20:37:09 +0200

    fix: 🐛preview-modal trailer shadow overflow

--- src/router/anime/preview-modal.tsx
+++ src/router/anime/preview-modal.tsx
@@ -32,7 +32,7 @@ left: 50%; */
   background-color: rgb(35, 35, 35);
   border-radius: 1rem;
   box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
-  /* overflow: hidden; */
+  overflow: hidden;
   /* overflow: auto; */
   /* position: fixed;
   top: 50%;
