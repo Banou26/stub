41cd6fc1dfe1ad16489d6dc07338a938462e802d
Banou <me@banou.dev>
2024-04-05 02:52:33 -0400

    refactor: ♻️update graph tests

--- src/test.ts
+++ src/test.ts
@@ -1,43 +1,13 @@
-import { makeInMemoryGraphDatabase, Node } from 'scannarr/src/urql/graph'
+import { makeGraphDatabase } from 'scannarr/src/graph-database/index'
 
-const recursiveRemoveNullable = (obj) =>
-  Array.isArray(obj)
-    ? obj.map(recursiveRemoveNullable)
-    : (
-      typeof obj === 'object'
-        ? (
-          Object
-            .fromEntries(
-              Object
-                .entries(obj)
-                .filter(([_, value]) => value !== null && value !== undefined)
-                .map(([key, value]) => [key, recursiveRemoveNullable(value)])
-            )
-        )
-        : obj
-    )
-
-type User = {
-  _id: string
-  __typename: 'User'
-  uri: string
-  foo: string
-  // friends: User[]
-  friends: UserNode[]
-}
-
-type UserNode = Node<User>
-
-type Nodes = User
-
-const db = makeInMemoryGraphDatabase<Nodes>({})
+const db = makeGraphDatabase()
 
 const user1 = db.insertOne({
   __typename: 'User',
   uri: 'user1',
   foo: 'foo1',
   friends: [] as UserNode[]
-}, { returnNode: true })
+})
 
 const user2 = db.insertOne({
   __typename: 'User',
@@ -45,28 +15,43 @@ const user2 = db.insertOne({
   foo: 'foo2',
   friends: []
   // friends: [user1]
-}, { returnNode: true })
+})
 
-db.updateOne({ _id: user1._id }, { $set: { friends: [user2] } })
+console.log('test query _id', db.findOne({ _id: user1._id }))
+
+db.updateOne({ _id: user1._id }, data => ({ ...data, friends: [user2] }))
 
 const userWithFriendsMap =
-  user1
-    .map((user) => ({
+  db
+    .mapOne({ _id: user1._id }, (user) => ({
       ...user,
       friends:
         user
-        .friends
-        .map(friend =>
-          friend.map(friend2 => ({
-            ...friend2,
-            friends: friend2.friends.map(friend3 => friend3.$)
-          }))
-        )
+          .friends
+          .map(friend =>
+            db.mapOne(
+              { _id: friend._id },
+              (friend) =>
+                db.mapOne(
+                  { _id: friend._id },
+                  (friend) => friend
+                )
+            )
+          )
+
+        // db.mapMany(
+        //   { _id: { $in: user.friends.map(friend => friend._id) } },
+        //   (friend) =>
+        //     db.mapOne(
+        //       { _id: friend._id },
+        //       (friend) => friend
+        //     )
+        // )
     }))
 
 userWithFriendsMap.subscribe(res => {
   console.log('userWithFriends 2222', res)
 })
 
-console.log('findOne', db.findOne({ uri: 'user1' }))
-db.updateOne({ _id: user2._id }, { $set: { friends: [user1] } })
+console.log('findOne uri', db.findOne({ uri: 'user1' }))
+db.updateOne({ _id: user2._id }, data => ({ ...data, friends: [user1] }))
