895d8d2529fbf6df564d12758ce09c3e0226a4c6
Banou26 <me@banou.dev>
2025-08-08 17:20:18 +0900

    refactor: ♻️improve setup scripts

--- src/worker/database/index.ts
+++ src/worker/database/index.ts
@@ -2,7 +2,7 @@ import { createWaSQLiteDB } from './drizzle-driver'
 export { WaSQLiteDrizzleDriver, createWaSQLiteDB } from './drizzle-driver'
 
 // @ts-expect-error
-import SQLInit from '../../../drizzle/0000_happy_scalphunter.sql?raw'
+import SQLInit from '../../../drizzle/0000_init.sql?raw'
 console.log('SQLInit', SQLInit)
 
 const db = await createWaSQLiteDB(
