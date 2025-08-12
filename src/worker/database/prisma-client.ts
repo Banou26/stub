c8de220f0c2d9336b23084f8e04a02774e101b16
Banou26 <me@banou.dev>
2025-08-12 19:08:07 +0900

    refactor: ♻️add prisma computed uid & uri fields

--- src/worker/database/prisma-client.ts
+++ src/worker/database/prisma-client.ts
@@ -4,7 +4,64 @@ import SQLSchema from '../../../prisma/migrations/0_init/migration.sql?raw'
 import { PrismaClient } from '../../../prisma/generated'
 
 const adapter = await createWaSQLitePrismaAdapter()
-const prismaClient = new PrismaClient({ adapter })
+const prismaClient =
+  new PrismaClient({
+    adapter,
+    omit: {
+      mediaTrailer: {
+        mediaUuid: true
+      },
+      mediaCover: {
+        mediaUuid: true
+      },
+      mediaBanner: {
+        mediaUuid: true
+      },
+      episode: {
+        mediaUuid: true
+      },
+      episodeThumbnail: {
+        episodeUuid: true
+      },
+      playbackSource: {
+        episodeUuid: true
+      }
+    } as const
+  })
+    .$extends({
+      result: {
+        media: {
+          uri: {
+            needs: { origin: true, id: true },
+            compute: (media) => `${media.origin}:${media.id}`
+          },
+          uid: {
+            needs: { origin: true, id: true, language: true },
+            compute: (media) => `${media.origin}:${media.language}:${media.id}`
+          }
+        },
+        episode: {
+          uri: {
+            needs: { origin: true, id: true },
+            compute: (episode) => `${episode.origin}:${episode.id}`
+          },
+          uid: {
+            needs: { origin: true, id: true, language: true },
+            compute: (episode) => `${episode.origin}:${episode.language}:${episode.id}`
+          }
+        },
+        playbackSource: {
+          uri: {
+            needs: { origin: true, id: true },
+            compute: (playbackSource) => `${playbackSource.origin}:${playbackSource.id}`
+          },
+          uid: {
+            needs: { origin: true, id: true, language: true },
+            compute: (playbackSource) => `${playbackSource.origin}:${playbackSource.language}:${playbackSource.id}`
+          }
+        }
+      }
+    })
 await prismaClient.$executeRawUnsafe(SQLSchema)
 
 export default prismaClient
