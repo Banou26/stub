47775acc8726c30984418d69f5e919280e688631
Banou <me@banou.dev>
2025-09-03 02:33:15 +0900

    refactor: ♻️re-impl draggable list

--- src/router/home/index.tsx
+++ src/router/home/index.tsx
@@ -4,12 +4,13 @@ import type { Get_Releasing_Media_PageSubscription } from '../../generated/graph
 import { css } from '@emotion/react'
 import { useSubscription } from 'urql'
 import { Grid } from 'react-window'
-import { useState } from 'preact/compat'
+import { useCallback, useState } from 'preact/compat'
 
-import { gql } from '../../generated/gql'
+import { gql } from '../../generated'
+import { getRoutePath, Route } from '../path'
 import { MediaSort, MediaStatus } from '../../generated/graphql'
 import MediaTitle from '../../components/media-title'
-import { getRoutePath, Route } from '../path'
+import Draggable from '../../components/draggable'
 
 const getReleasingMediaPage = gql(`
   subscription GET_RELEASING_MEDIA_PAGE($input: MediaPageInput!) {
@@ -44,14 +45,30 @@ const getReleasingMediaPage = gql(`
 `)
 
 const style = css`
-  .list {
-      display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-      grid-gap: 1rem;
+.section {
+  display: flex;
+  flex-direction: column;
+  & > .title {
+    margin-left: 10rem;
+    font-size: 4rem;
+    font-weight: bold;
+    color: rgb(255, 255, 255);
+    text-shadow: rgba(0, 0, 0, 0.8) -1px -1px 0px, rgba(0, 0, 0, 0.8) -1px 1px 0px, rgba(0, 0, 0, 0.8) 1px -1px 0px, rgba(0, 0, 0, 0.8) 1px 1px 0px;
+    padding-bottom: 0.5rem;
+    user-select: none;
   }
+  & > div > .list {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    grid-gap: 1rem;
+    padding-bottom: 0.5rem;
+    scrollbar-width: thin;
+    scrollbar-color: rgb(134, 132, 141) rgb(15, 15, 15);
+  }
+}
 `
 
-export default () => {
+const Index = () => {
   const [{ data }] = useSubscription({
     query: getReleasingMediaPage,
     variables: {
@@ -64,37 +81,39 @@ export default () => {
 
   const mediaNodes = data?.mediaPage?.nodes || []
 
-  console.log('mediaNodes', mediaNodes)
-  const matchedNodes = mediaNodes.filter(media => media.uri?.includes('anilist:185660') || media.uri?.includes('mal:60543'))
-  console.log('matchedNodes', matchedNodes)
-
   const [visibleStartIndex, setVisibleStartIndex] = useState(0)
-  const CellComponent = ({ mediaNodes, columnIndex, style }: CellComponentProps<{ mediaNodes: Get_Releasing_Media_PageSubscription['mediaPage']['nodes'] }>) => {
+  const CellComponent = useCallback(({ mediaNodes, columnIndex, style }: CellComponentProps<{ mediaNodes: Get_Releasing_Media_PageSubscription['mediaPage']['nodes'] }>) => {
     const media = mediaNodes[columnIndex]
     if (!media) return null
 
-    const left = Number(style.left || 0) + (columnIndex - visibleStartIndex) * 10
+    const left = Number(style.left || 0) + (columnIndex - visibleStartIndex) * 10 + 100 /* left margin */
 
     return <MediaTitle media={media} to={getRoutePath(Route.TITLE, { uri: media.uri })} style={{ ...style, left }}/>
-  }
+  }, [])
+
+  const [isDragging, setIsDragging] = useState(false)
 
   return (
     <div css={style}>
-      Home
-      <div>
-        <span>Current Season</span>
-        <Grid
-          className='list'
-          cellComponent={CellComponent}
-          cellProps={{ mediaNodes }}
-          columnCount={mediaNodes.length}
-          columnWidth={250}
-          rowCount={1}
-          rowHeight={350}
-          overscanCount={5}
-          onCellsRendered={({ columnStartIndex }) => setVisibleStartIndex(columnStartIndex)}
-        />
+      <div className='section'>
+        <span className='title'>Current Season</span>
+        <Draggable isDragging={isDragging} setIsDragging={setIsDragging}>
+          <Grid
+            className='list'
+            cellComponent={CellComponent}
+            cellProps={{ mediaNodes }}
+            columnCount={mediaNodes.length}
+            columnWidth={250}
+            rowCount={1}
+            rowHeight={350}
+            overscanCount={5}
+            onCellsRendered={({ columnStartIndex }) => setVisibleStartIndex(columnStartIndex)}
+          />
+        </Draggable>
+
       </div>
     </div>
   )
 }
+
+export default Index
